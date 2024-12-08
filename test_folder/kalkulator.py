1993280ef257d994321da0fa6f498408fec881a6
ahmadgary <ahmadgary02022005@gmail.com>
2024-12-08 09:47:03 +0700

    Add existing file

--- test_folder/kalkulator.py
+++ test_folder/kalkulator.py
@@ -20,6 +20,12 @@ def clear():
     entrada.delete(0, tk.END)
     label_hasil.config(text="Hasil:", fg="white")
 
+def backspace():
+    current_text = entrada.get()
+    new_text = current_text[:-1]
+    entrada.delete(0, tk.END)
+    entrada.insert(0, new_text)
+
 root = tk.Tk()
 root.title("Kalkulator")
 
@@ -32,18 +38,18 @@ entry_text_color = "#ecf0f1"
 result_text_color = "#ecf0f1"
 
 # Tampilan Entri dan Hasil
-entrada = tk.Entry(root, width=25, font=("Arial", 18), bg=entry_bg_color, fg=entry_text_color)
-entrada.grid(row=0, column=0, columnspan=4, pady=10)
+entrada = tk.Entry(root, font=("Arial", 18), bg=entry_bg_color, fg=entry_text_color)
+entrada.grid(row=0, column=0, columnspan=4, pady=10, sticky="nsew")
 
 label_hasil = tk.Label(root, text="Hasil:", font=("Arial", 18), bg="#2c3e50", fg=result_text_color)
-label_hasil.grid(row=1, column=0, columnspan=4, pady=10)
+label_hasil.grid(row=1, column=0, columnspan=4, pady=10, sticky="nsew")
 
 # Tombol Angka dan Operasi
 buttons = [
     '7', '8', '9', '+',
     '4', '5', '6', '-',
     '1', '2', '3', '*',
-    'C', '0', '.', '='
+    'C', '0', '.', '=', '(', ')'
 ]
 
 row = 2
@@ -52,16 +58,25 @@ col = 0
 for button in buttons:
     if button == '=':
         tk.Button(root, text=button, width=5, height=2, font=("Arial", 18),
-                  command=eval_expression, bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5)
+                  command=eval_expression, bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5, sticky="nsew")
     elif button == 'C':
         tk.Button(root, text=button, width=5, height=2, font=("Arial", 18),
-                  command=clear, bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5)
+                  command=clear, bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5, sticky="nsew")
     else:
         tk.Button(root, text=button, width=5, height=2, font=("Arial", 18),
-                  command=lambda button=button: click(button), bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5)
+                  command=lambda button=button: click(button), bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5, sticky="nsew")
     col += 1
     if col > 3:
         col = 0
         row += 1
+
+# Tombol Backspace
+tk.Button(root, text='⌫', width=5, height=2, font=("Arial", 18), command=backspace, bg=button_color, fg=button_text_color).grid(row=row, column=col, pady=5, padx=5, sticky="nsew")
+
+# Konfigurasi pengaturan grid untuk mendukung resizing
+for i in range(4):
+    root.grid_columnconfigure(i, weight=1)
+for i in range(row+1):
+    root.grid_rowconfigure(i, weight=1)
 
 root.mainloop()
