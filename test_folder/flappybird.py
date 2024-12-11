3e6cd5c89063d558cef9dcbd47fb2de2d5072e84
ahmadgary <ahmadgary02022005@gmail.com>
2024-12-11 21:07:34 +0700

    Add existing file

--- test_folder/flappybird.py
+++ test_folder/flappybird.py
@@ -6,9 +6,9 @@ import random
 layar_lebar = 800  # Perbesar lebar layar
 layar_tinggi = 800  # Perbesar tinggi layar
 pipi_lebar = 100  # Perbesar lebar pipa
-pipi_tinggi_min = 100  # Tinggi minimum pipa
-pipi_tinggi_max = 450  # Tinggi maksimum pipa
-jarak_pipa = 20  # Jarak antara pipa atas dan pipa bawah (diperkecil)
+pipi_tinggi = 200  # Tinggi pipa default
+jarak_pipa_vertikal = 100  # Jarak vertikal antara pipa atas dan pipa bawah
+jarak_pipa_horizontal = 400  # Jarak horizontal antara pipa yang berturut-turut
 burung_ukuran = 50  # Perbesar ukuran burung
 gravitasi = 0.2
 kecepatan_lompat = -4
@@ -49,14 +49,19 @@ class FlappyBird:
         self.burung_y = layar_tinggi // 2
         self.burung_vy = 0
 
-        self.pipi_x = layar_lebar
-        self.update_pipes()
+        self.pipi_x1 = layar_lebar
+        self.pipi_x2 = layar_lebar + jarak_pipa_horizontal
+
+        self.pipi_y_atas1, self.pipi_y_bawah1 = self.generate_pipe_heights()
+        self.pipi_y_atas2, self.pipi_y_bawah2 = self.generate_pipe_heights()
 
         self.skor = 0
 
         self.burung = self.canvas.create_image(self.burung_x, self.burung_y, anchor="nw", image=self.burung_image)
-        self.pipi_atas = self.canvas.create_rectangle(self.pipi_x, 0, self.pipi_x + pipi_lebar, self.pipi_y_atas, fill="grey")
-        self.pipi_bawah = self.canvas.create_rectangle(self.pipi_x, self.pipi_y_bawah, self.pipi_x + pipi_lebar, layar_tinggi, fill="grey")
+        self.pipi1_atas = self.canvas.create_rectangle(self.pipi_x1, 0, self.pipi_x1 + pipi_lebar, self.pipi_y_atas1, fill="grey")
+        self.pipi1_bawah = self.canvas.create_rectangle(self.pipi_x1, self.pipi_y_bawah1, self.pipi_x1 + pipi_lebar, layar_tinggi, fill="grey")
+        self.pipi2_atas = self.canvas.create_rectangle(self.pipi_x2, 0, self.pipi_x2 + pipi_lebar, self.pipi_y_atas2, fill="grey")
+        self.pipi2_bawah = self.canvas.create_rectangle(self.pipi_x2, self.pipi_y_bawah2, self.pipi_x2 + pipi_lebar, layar_tinggi, fill="grey")
         self.teks_skor = self.canvas.create_text(layar_lebar // 2, 50, text="Skor: 0", font=("Arial", 24))
 
     def lompat(self, event):
@@ -67,29 +72,41 @@ class FlappyBird:
         self.burung_vy += gravitasi
         self.canvas.coords(self.burung, self.burung_x, self.burung_y)
 
-        self.pipi_x -= 3  # Tambahkan sedikit kecepatan pipa agar seimbang dengan layar yang lebih besar
-        self.canvas.coords(self.pipi_atas, self.pipi_x, 0, self.pipi_x + pipi_lebar, self.pipi_y_atas)
-        self.canvas.coords(self.pipi_bawah, self.pipi_x, self.pipi_y_bawah, self.pipi_x + pipi_lebar, layar_tinggi)
+        self.pipi_x1 -= 3  # Pindahkan pipa pertama ke kiri
+        self.pipi_x2 -= 3  # Pindahkan pipa kedua ke kiri
+        self.canvas.coords(self.pipi1_atas, self.pipi_x1, 0, self.pipi_x1 + pipi_lebar, self.pipi_y_atas1)
+        self.canvas.coords(self.pipi1_bawah, self.pipi_x1, self.pipi_y_bawah1, self.pipi_x1 + pipi_lebar, layar_tinggi)
+        self.canvas.coords(self.pipi2_atas, self.pipi_x2, 0, self.pipi_x2 + pipi_lebar, self.pipi_y_atas2)
+        self.canvas.coords(self.pipi2_bawah, self.pipi_x2, self.pipi_y_bawah2, self.pipi_x2 + pipi_lebar, layar_tinggi)
 
-        if self.pipi_x < -pipi_lebar:
-            self.pipi_x = layar_lebar
-            self.update_pipes()
+        if self.pipi_x1 < -pipi_lebar:
+            self.pipi_x1 = layar_lebar
+            self.pipi_y_atas1, self.pipi_y_bawah1 = self.generate_pipe_heights()
+            self.skor += 1
+            self.canvas.itemconfig(self.teks_skor, text=f"Skor: {self.skor}")
+
+        if self.pipi_x2 < -pipi_lebar:
+            self.pipi_x2 = layar_lebar
+            self.pipi_y_atas2, self.pipi_y_bawah2 = self.generate_pipe_heights()
             self.skor += 1
             self.canvas.itemconfig(self.teks_skor, text=f"Skor: {self.skor}")
 
         if (self.burung_y < 0 or self.burung_y > layar_tinggi - burung_ukuran or
-            (self.burung_x + burung_ukuran > self.pipi_x and self.burung_x < self.pipi_x + pipi_lebar and
-             (self.burung_y < self.pipi_y_atas or self.burung_y + burung_ukuran > self.pipi_y_bawah))):
+            (self.burung_x + burung_ukuran > self.pipi_x1 and self.burung_x < self.pipi_x1 + pipi_lebar and
+             (self.burung_y < self.pipi_y_atas1 or self.burung_y + burung_ukuran > self.pipi_y_bawah1)) or
+            (self.burung_x + burung_ukuran > self.pipi_x2 and self.burung_x < self.pipi_x2 + pipi_lebar and
+             (self.burung_y < self.pipi_y_atas2 or self.burung_y + burung_ukuran > self.pipi_y_bawah2))):
             self.canvas.itemconfig(self.teks_skor, text=f"Game Over! Skor: {self.skor}")
             self.root.unbind("<space>")
             self.tampilkan_tombol_ulangi()
         else:
             self.root.after(16, self.update)
 
-    def update_pipes(self):
-        # Mengatur ukuran pipa secara acak
-        self.pipi_y_atas = random.randint(50, layar_tinggi - pipi_tinggi_min - jarak_pipa)
-        self.pipi_y_bawah = self.pipi_y_atas + random.randint(pipi_tinggi_min, pipi_tinggi_max) + jarak_pipa
+    def generate_pipe_heights(self):
+        # Mengatur posisi pipa dengan jarak vertikal tetap
+        pipa_y_atas = random.randint(50, layar_tinggi - pipi_tinggi - jarak_pipa_vertikal)
+        pipa_y_bawah = pipa_y_atas + pipi_tinggi + jarak_pipa_vertikal
+        return pipa_y_atas, pipa_y_bawah
 
     def tampilkan_tombol_ulangi(self):
         self.tombol_ulangi = tk.Button(self.root, text="Restart", command=self.restart_game, font=("Arial", 14), bg="red", fg="white")
